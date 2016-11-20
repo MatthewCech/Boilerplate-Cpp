51e28ba19d51cb6d2d8f1b2929a5c029db0f3b40
GHOST\rever <reveriewisp@gmail.com>
2016-11-20 15:06:06 -0800

    + Added example usage of external headers

--- Source/main.cpp
+++ Source/main.cpp
@@ -8,13 +8,23 @@ The application entry point and boilerplate main.
 
 @copyright "Don't be a Jerk" (See LICENSE.md)
 *****************************************************************************/
-#include <iostream> // std::cout
+#include <iostream>               // std::cout
+#include <RUtils\RException.hpp>  // Rutils::RException
 
 
 
 // Application entry point
-int main(int arc, char** argv)
+int main(int argc, char** argv) try
 {
-  std::cout << "Hello world!" << std::endl;
+  if (argc == 1)
+    std::cout << "Hello world!" << std::endl;
+  else
+    throw RUtils::RException("More than one argument!");
+
+  // Return normally.
   return 0;
+}
+catch(RUtils::RException e)
+{
+  std::cout << e << std::endl;
 }
