a29a6bc12887ac32cccd37db4e44b35e42e2555a
Reverie Wisp <reveriewisp@gmail.com>
2016-11-20 14:46:57 -0800

    + Added default main

---
+++ Source/main.cpp
@@ -0,0 +1,20 @@
+/*!***************************************************************************
+@file    main.cpp
+@author  Reverie Wisp
+@date    11/20/2016
+
+@brief 
+The application entry point and boilerplate main. 
+
+@copyright "Don't be a Jerk" (See LICENSE.md)
+*****************************************************************************/
+#include <iostream> // std::cout
+
+
+
+// Application entry point
+int main(int arc, char** argv)
+{
+  std::cout << "Hello world!" << std::endl;
+  return 0;
+}
